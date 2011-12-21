fd7ccd78f5b8ea563a43a59b06b7c4555a6d7869
Alexander Stehlik <alexander.stehlik@googlemail.com>
2011-12-21 09:27:57 +0100

    Initial version

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "formhandler".
+#
+# Auto generated 28-11-2011 17:32
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Formhandler Subscription',
+	'description' => 'Provides additional classes for the formhandler extension to build (newsletter) subscribe and modify / unsubscripe forms. It comes with some YAML based example templates.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => 'formhandler',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Alexander Stehlik',
+	'author_email' => 'alexander.stehlik.deleteme@googlemail.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.3.0-0.0.0',
+			'typo3' => '4.6.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
