15aa9d56913c45e851c63c26429426f524ce792d
Alexander Stehlik <alexander.stehlik@googlemail.com>
2012-10-13 18:48:55 +0200

    PHP Version 5.2 is now required
    Thanks to Urs Bräm for testing and feedback!

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.0-0.0.0',
+			'php' => '5.2.0-0.0.0',
 			'typo3' => '4.5.0-0.0.0',
 			'formhandler' => '1.4.0-0.0.0',
 		),
