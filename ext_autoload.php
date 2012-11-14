643c70150dd81cd11b1dc8accca6163d0a79dcfd
Alexander Stehlik <alexander.stehlik@googlemail.com>
2012-11-14 15:20:06 +0100

    Added new alpha version of extbase processors
    * added extbase finisher, interceptor and pre-processor
    * added a typoscript utility class

--- ext_autoload.php
+++ ext_autoload.php
@@ -6,5 +6,7 @@
 $classesPath = t3lib_extMgm::extPath('formhandler_subscription', 'Classes/');
 return array(
 	'tx_formhandlersubscription_api_subscriptionapi'	=> $classesPath . 'API/Tx_FormhandlerSubscription_API_SubscriptionAPI.php',
+	'tx_formhandlersubscription_mvc_formhandlerdata'	=> $classesPath . 'Mvc/FormhandlerData.php',
+	'tx_formhandlersubscription_utils_typoscript'		=> $classesPath . 'Utils/Tx_FormhandlerSubscription_Utils_TypoScript.php',
 );
 ?>
